8cacbefea99209aabd26765f7dd4a9ce72918e7b
alexandrefonseca1972 <alexandrefonseca1972@gmail.com>
2026-09-17 10:18:17 -0400

    Add shadcn, zod, firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,22 @@
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { z } from "zod";
+
+const config = z
+  .object({
+    apiKey: z.string().min(1),
+    authDomain: z.string().min(1),
+    projectId: z.string().min(1),
+    storageBucket: z.string().min(1),
+    messagingSenderId: z.string().min(1),
+    appId: z.string().min(1),
+  })
+  .parse({
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  });
+
+export const app = getApps().length ? getApp() : initializeApp(config);
